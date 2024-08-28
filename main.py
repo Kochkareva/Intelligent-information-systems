3fbc24f38ceda37071e56d0842643289deada81e
Kochkareva <lisskim.1821@gmail.com>
2024-08-28 13:05:20 +0400

    lw 7

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
-from sklearn.metrics import recall_score, precision_score
-from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-import os.path
-import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.preprocessing import StandardScaler
+import numpy as np
+import tensorflow as tf
 
 
-picfld = os.path.join('static', 'charts')
+def recurrent_neural_network():
+    # Загрузка текстового файла и предварительная обработка данных
+    with open('V3001TH2.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
 
-data = pd.read_csv('D:/Интеллектуальные информационные системы/Dataset/updated_job_descriptions.csv')
-y = data['Country']
+    chars = sorted(list(set(text)))
+    char_to_index = {char: index for index, char in enumerate(chars)}
+    index_to_char = {index: char for index, char in enumerate(chars)}
 
+    num_chars = len(chars)
+    text_length = len(text)
 
-def MLP_classifier_country():
-    df = data.copy()
-    df.drop(['Country', 'location', 'Company Size', 'Preference', 'Job Title', 'Role', 'Job Portal',
-             'skills', 'Company', 'Min Experience', 'Max Experience', 'Min Salary',
-             'Max Salary', 'Sector', 'Industry', 'City', 'State', 'Ticker', 'year', 'month', 'day'],
-            axis=1, inplace=True)
-    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2)
-    mlp = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=20)
-    scaler = StandardScaler()
-    scaler.fit(X_train.values)
-    X_train_scaler = scaler.transform(X_train.values)
-    X_test_scaler = scaler.transform(X_test.values)
-    mlp.fit(X_train_scaler, y_train)
-    y_pred = mlp.predict(X_test_scaler)
-    precision = precision_score(y_test.values, y_pred, average='weighted')
-    recall = recall_score(y_test.values, y_pred, average='weighted')
-    print("Precision:", precision)
-    print("Recall:", recall)
+    # Генерация тренировочных данных
+    seq_length = 100  # Длина входной последовательности
+    train_x = []
+    train_y = []
+    for i in range(0, text_length - seq_length, 1):
+        input_seq = text[i:i + seq_length]
+        output_seq = text[i + seq_length]
+        train_x.append([char_to_index[char] for char in input_seq])
+        train_y.append(char_to_index[output_seq])
 
-    # Получаем метки классов
-    class_labels = mlp.classes_
-    print("Class labels:", class_labels)
-    print("Уникальных Country :", data['Country'].nunique())
+    train_x = np.reshape(train_x, (len(train_x), seq_length, 1))
+    train_x = train_x / float(num_chars)
+    train_y = tf.keras.utils.to_categorical(train_y)
 
-    # Создаем график
-    plt.scatter(X_train['Qualifications'].values, X_train['Work Type'].values, c=y_train.values, cmap='viridis', label='Train Data')
-    plt.scatter(X_test['Qualifications'].values, X_test['Work Type'].values, c=y_test.values, cmap='viridis', marker='x', label='Test Data')
-    plt.xlabel('Qualifications')
-    plt.ylabel('Work Type')
-    plt.title('MLPClassifier Visualization')
-    plt.savefig('static/charts/MLPClassifier.png')
-    plt.close()
-    return 0
+    model = tf.keras.Sequential([
+        tf.keras.layers.LSTM(128, input_shape=(train_x.shape[1], train_x.shape[2])),
+        tf.keras.layers.Dense(num_chars, activation='softmax')
+    ])
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    # Обучение модели
+    model.fit(train_x, train_y, epochs=80, batch_size=128)
+
+    # Генерация текста
+    start_index = np.random.randint(0, len(train_x) - 1)
+    start_seq = train_x[start_index]
+
+    generated_text = ''
+    for _ in range(500):
+        x = np.reshape(start_seq, (1, len(start_seq), 1))
+        x = x / float(num_chars)
+
+        prediction = model.predict(x, verbose=0)
+        index = np.argmax(prediction)
+        result = index_to_char[index]
+
+        generated_text += result
+        start_seq = np.append(start_seq, index)
+        start_seq = start_seq[1:]
+
+    with open('сгенерированный_текст.txt', 'w', encoding='utf-8') as f:
+        f.write(generated_text)
 
 
 if __name__ == '__main__':
-    MLP_classifier_country()
+    recurrent_neural_network()
+
