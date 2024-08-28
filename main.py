9fefb7a3dbaf40cc984dffa160cd94e7d19e87bb
Kochkareva <lisskim.1821@gmail.com>
2024-08-28 13:00:50 +0400

    lw 3

--- main.py
+++ main.py
@@ -1,88 +1,194 @@
-from operator import itemgetter
 import numpy as np
-from sklearn.feature_selection import RFE, f_regression
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import LinearRegression
-
-# генерируем исходные данные: 750 строк-наблюдений и 14 столбцов-признаков
-np.random.seed(0)
-size = 750
-X = np.random.uniform(0, 1, (size, 14))
-# Задаем функцию-выход: регрессионную проблему Фридмана
-Y = (10 * np.sin(np.pi*X[:, 0]*X[:, 1]) + 20*(X[:, 2] - .5)**2 + 10*X[:, 3] + 5*X[:, 4]**5 + np.random.normal(0, 1))
-# Добавляем зависимость признаков
-X[:, 10:] = X[:, :4] + np.random.normal(0, .025, (size, 4))
-names = ["x%s" % i for i in range(1, 15)]  # - список признаков вида ['x1', 'x2', 'x3', ..., 'x14']
-ranks = dict()
+from sklearn.tree import DecisionTreeClassifier
 
 
-def rank_to_dict(ranks, names):
-     # получение абсолютных значений оценок(модуля)
-     ranks = np.abs(ranks)
-     minmax = MinMaxScaler()
-     # преобразование данных
-     ranks = minmax.fit_transform(np.array(ranks).reshape(14, 1)).ravel()
-     # округление элементов массива
-     ranks = map(lambda x: round(x, 2), ranks)
-     # преобразование данных
-     return dict(zip(names, ranks))
+def data_preprocessing():
+    df_job_orig = pd.read_csv('D:/Интеллектуальные информационные системы/Dataset/job_descriptions.csv')
+    df_job_orig = pd.DataFrame(df_job_orig)
+    desired_rows = int(0.99 * len(df_job_orig))
+    df_job = df_job_orig.copy()
+    df_job = df_job[:desired_rows]
+    df_job.drop(["Job Id", "latitude", "longitude", "Contact Person", "Contact", "Job Description", "Responsibilities"], axis=1,
+                inplace=True)
+    # digitization
+    # --------------------------'Years'------------------------
+    # Разделяем значения 'Years' на минимальное и максимальное
+    # Удаляем символы валюты и другие символы
+    df_job['Experience'] = df_job['Experience'].apply(lambda x: str(x).replace('Years', '') if x is not None else x)
+    df_job[['Min Experience', 'Max Experience']] = df_job['Experience'].str.split(' to ', expand=True)
+    # Преобразуем значения в числовой формат
+    df_job['Min Experience'] = pd.to_numeric(df_job['Min Experience'])
+    df_job['Max Experience'] = pd.to_numeric(df_job['Max Experience'])
+    # --------------------------'Salary Range'------------------------
+    # Удаляем символы валюты и другие символы
+    df_job['Salary Range'] = df_job['Salary Range'].str.replace('$', '').str.replace('K', '000')
+    # Разделяем значения на минимальное и максимальное
+    df_job[['Min Salary', 'Max Salary']] = df_job['Salary Range'].str.split('-', expand=True)
+    # Преобразуем значения в числовой формат
+    df_job['Min Salary'] = pd.to_numeric(df_job['Min Salary'])
+    df_job['Max Salary'] = pd.to_numeric(df_job['Max Salary'])
+    # --------------------------'Qualifications'------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    qualifications_dict = {qual: i for i, qual in enumerate(df_job['Qualifications'].unique())}
+    # Заменяем значения в столбце "Qualifications" соответствующими числовыми идентификаторами
+    df_job['Qualifications'] = df_job['Qualifications'].map(qualifications_dict)
+    # --------------------------'location'------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    locations_dict = {locat: i for i, locat in enumerate(df_job['location'].unique())}
+    # Заменяем значения в столбце "location" соответствующими числовыми идентификаторами
+    df_job['location'] = df_job['location'].map(locations_dict)
+    # --------------------------'Country'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    countries_dict = {countr: i for i, countr in enumerate(df_job['Country'].unique())}
+    # Заменяем значения в столбце "Country" соответствующими числовыми идентификаторами
+    df_job['Country'] = df_job['Country'].map(countries_dict)
+    # --------------------------'Work Type'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    wt_dict = {wt: i for i, wt in enumerate(df_job['Work Type'].unique())}
+    # Заменяем значения в столбце "Work Type" соответствующими числовыми идентификаторами
+    df_job['Work Type'] = df_job['Work Type'].map(wt_dict)
+    # --------------------------'Preference gender'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    gender_dict = {gender: i for i, gender in enumerate(df_job['Preference'].unique())}
+    # Заменяем значения в столбце "Preference" соответствующими числовыми идентификаторами
+    df_job['Preference'] = df_job['Preference'].map(gender_dict)
+    # --------------------------'Job Title'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    jt_dict = {jt: i for i, jt in enumerate(df_job['Job Title'].unique())}
+    # Заменяем значения в столбце "Job Title" соответствующими числовыми идентификаторами
+    df_job['Job Title'] = df_job['Job Title'].map(jt_dict)
+    # --------------------------'Role'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    role_dict = {role: i for i, role in enumerate(df_job['Role'].unique())}
+    # Заменяем значения в столбце "Role" соответствующими числовыми идентификаторами
+    df_job['Role'] = df_job['Role'].map(role_dict)
+    # --------------------------'Job Portal'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    jp_dict = {jp: i for i, jp in enumerate(df_job['Job Portal'].unique())}
+    # Заменяем значения в столбце "Job Portal" соответствующими числовыми идентификаторами
+    df_job['Job Portal'] = df_job['Job Portal'].map(jp_dict)
+    # --------------------------'Company'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {comp: i for i, comp in enumerate(df_job['Company'].unique())}
+    # Заменяем значения в столбце "Company" соответствующими числовыми идентификаторами
+    df_job['Company'] = df_job['Company'].map(comp_dict)
+    # --------------------------'Company Profile'-------------------------
+    df_company_profile = df_job['Company Profile'].str.split('",', expand=True)
+    df_company_profile.columns = ['Sector', 'Industry', 'City', 'State', 'Zip', 'Website', 'Ticker', 'CEO']
+    df_company_profile = df_company_profile.apply(
+        lambda x: x.str.replace('{', '').str.replace('"', '').str.replace('}', '')
+        .str.replace('Sector', '').str.replace('Industry', '').str.replace('City', '')
+        .str.replace('State', '').str.replace('Zip', '').str.replace('Website', '')
+        .str.replace('Ticker', '').str.replace('CEO', '').str.replace(':', ''))
+    df_company_profile.drop(["CEO", "Website", "Zip"], axis=1, inplace=True)
+        # --------------------------'Sector'-------------------------
+        # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {sector: i for i, sector in enumerate(df_company_profile['Sector'].unique())}
+        # Заменяем значения в столбце "Sector" соответствующими числовыми идентификаторами
+    df_company_profile['Sector'] = df_company_profile['Sector'].map(comp_dict)
+        # --------------------------'Industry'-------------------------
+        # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {industry: i for i, industry in enumerate(df_company_profile['Industry'].unique())}
+        # Заменяем значения в столбце "Industry" соответствующими числовыми идентификаторами
+    df_company_profile['Industry'] = df_company_profile['Industry'].map(comp_dict)
+        # --------------------------'City'-------------------------
+        # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {city: i for i, city in enumerate(df_company_profile['City'].unique())}
+        # Заменяем значения в столбце "City" соответствующими числовыми идентификаторами
+    df_company_profile['City'] = df_company_profile['City'].map(comp_dict)
+        # --------------------------'State'-------------------------
+        # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {state: i for i, state in enumerate(df_company_profile['State'].unique())}
+        # Заменяем значения в столбце "State" соответствующими числовыми идентификаторами
+    df_company_profile['State'] = df_company_profile['State'].map(comp_dict)
+        # --------------------------'Ticker'-------------------------
+        # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {ticker: i for i, ticker in enumerate(df_company_profile['Ticker'].unique())}
+        # Заменяем значения в столбце "Ticker" соответствующими числовыми идентификаторами
+    df_company_profile['Ticker'] = df_company_profile['Ticker'].map(comp_dict)
+    # Объединение преобразованных столбцов с исходным датасетом
+    df_job = pd.concat([df_job, df_company_profile], axis=1)
+    # --------------------------'Job Posting Date'-------------------------
+    df_job[['year', 'month', 'day']] = df_job['Job Posting Date'].str.split('-', expand=True)
+    df_job['year'] = pd.to_numeric(df_job['year'])
+    df_job['month'] = pd.to_numeric(df_job['month'])
+    df_job['day'] = pd.to_numeric(df_job['day'])
+    # --------------------------'Benefits'-------------------------
+    df_job['Benefits'] = df_job['Benefits'].str.replace('{', '').str.replace('}', '')
+    # Применить метод get_dummies для оцифровки столбца 'Benefits'
+    benefits_encoded = pd.get_dummies(df_job['Benefits'], dtype=int)
+    # Соединить исходный DataFrame с оцифрованными данными
+    df_job = pd.concat([df_job, benefits_encoded], axis=1)
+    # --------------------------'skills'-------------------------
+    # Создаем словарь для отображения уникальных значений в числовые идентификаторы
+    comp_dict = {skill: i for i, skill in enumerate(df_job['skills'].unique())}
+    # Заменяем значения в столбце "skills" соответствующими числовыми идентификаторами
+    df_job['skills'] = df_job['skills'].map(comp_dict)
+    df_job.drop(["Company Profile", "Experience", "Salary Range", "Benefits", "Job Posting Date"], axis=1, inplace=True)
+    print(df_job.dtypes)
+    df_job.to_csv('D:/Интеллектуальные информационные системы/Dataset/updated_job_descriptions.csv', index=False)
 
 
-# Модель: случайное Лассо (RandomizedLasso) - устаревшее, поэтому используем Ridge-регрессия (Ridge Regression)
-def ridge_regressions():
-    # Создание экземпляра модели Ridge
-    ridge_model = Ridge()
-    ridge_model.fit(X, Y)
-    ranks['Ridge'] = rank_to_dict(ridge_model.coef_, names)
+def decision_tree_classifier():
+    data = pd.read_csv('D:/Интеллектуальные информационные системы/Dataset/updated_job_descriptions.csv')
+    corr = data[['location', 'Country', 'Work Type', 'Company Size', 'Preference', 'Job Title', 'Role', 'Job Portal', 'skills', 'Company', 'Min Experience', 'Max Experience', 'Min Salary',
+                 'Max Salary', 'Sector', 'Industry', 'City', 'State', 'Ticker', 'year', 'month', 'day',
+                 "'Casual Dress Code, Social and Recreational Activities, Employee Referral Programs, Health and Wellness Facilities, Life and Disability Insurance'",
+                 "'Childcare Assistance, Paid Time Off (PTO), Relocation Assistance, Flexible Work Arrangements, Professional Development'",
+                 "'Employee Assistance Programs (EAP), Tuition Reimbursement, Profit-Sharing, Transportation Benefits, Parental Leave'",
+                 "'Employee Referral Programs, Financial Counseling, Health and Wellness Facilities, Casual Dress Code, Flexible Spending Accounts (FSAs)'",
+                 "'Flexible Spending Accounts (FSAs), Relocation Assistance, Legal Assistance, Employee Recognition Programs, Financial Counseling'",
+                 "'Health Insurance, Retirement Plans, Flexible Work Arrangements, Employee Assistance Programs (EAP), Bonuses and Incentive Programs'",
+                 "'Health Insurance, Retirement Plans, Paid Time Off (PTO), Flexible Work Arrangements, Employee Assistance Programs (EAP)'",
+                 "'Legal Assistance, Bonuses and Incentive Programs, Wellness Programs, Employee Discounts, Retirement Plans'",
+                 "'Life and Disability Insurance, Stock Options or Equity Grants, Employee Recognition Programs, Health Insurance, Social and Recreational Activities'",
+                 "'Transportation Benefits, Professional Development, Bonuses and Incentive Programs, Profit-Sharing, Employee Discounts'",
+                 "'Tuition Reimbursement, Stock Options or Equity Grants, Parental Leave, Wellness Programs, Childcare Assistance'"]]
+    print(corr.head())
+    # определение целевой переменной
+    y = data['Qualifications']
+    # Разделение данных на обучающую и тестовую выборки
+    X_train, X_test, y_train, y_test = train_test_split(corr.values, y.values, test_size=0.2)
+    # Создание экземпляра классификатора дерева решений
+    clf = DecisionTreeClassifier(random_state=241)
+    # Обучение модели на обучающей выборке
+    clf.fit(X_train, y_train)
+    # Прогнозирование классов для тестовой выборки
+    y_pred = clf.predict(X_test)
+    importances = clf.feature_importances_
+    print("Важность признаков: ")
+    print(importances)
+    print("Отсортированная важность признаков: ")
+    conversion_ratings(importances)
 
 
-# Модель: рекурсивное сокращение признаков (Recursive Feature Elimination – RFE)
-def recursive_feature_elimination():
-    # создание модели LinearRegression
-    estimator = LinearRegression()
-    # создание модели RFE
-    rfe_model = RFE(estimator)
-    rfe_model.fit(X, Y)
-    ranks['Recursive Feature Elimination'] = rank_to_dict_rfe(rfe_model.ranking_, names)
-
-
-def rank_to_dict_rfe(ranking, names):
-    # нахождение обратных значений рангов
-    n_ranks = [float(1 / i) for i in ranking]
-    # округление элементов массива
-    n_ranks = map(lambda x: round(x, 2), n_ranks)
-    # преобразование данных
-    return dict(zip(names, n_ranks))
-
-
-# Модель: линейная корреляция (f_regression)
-def linear_correlation():
-    # вычисление линейной корреляции между X и y
-    correlation, p_values = f_regression(X, Y)
-    ranks['linear correlation'] = rank_to_dict(correlation, names)
+def conversion_ratings(rank):
+    column_names = ['location', 'Country', 'Work Type', 'Company Size', 'Preference', 'Job Title', 'Role', 'Job Portal', 'skills', 'Company', 'Min Experience', 'Max Experience', 'Min Salary',
+                 'Max Salary', 'Sector', 'Industry', 'City', 'State', 'Ticker', 'year', 'month', 'day',
+                 "'Casual Dress Code, Social and Recreational Activities, Employee Referral Programs, Health and Wellness Facilities, Life and Disability Insurance'",
+                 "'Childcare Assistance, Paid Time Off (PTO), Relocation Assistance, Flexible Work Arrangements, Professional Development'",
+                 "'Employee Assistance Programs (EAP), Tuition Reimbursement, Profit-Sharing, Transportation Benefits, Parental Leave'",
+                 "'Employee Referral Programs, Financial Counseling, Health and Wellness Facilities, Casual Dress Code, Flexible Spending Accounts (FSAs)'",
+                 "'Flexible Spending Accounts (FSAs), Relocation Assistance, Legal Assistance, Employee Recognition Programs, Financial Counseling'",
+                 "'Health Insurance, Retirement Plans, Flexible Work Arrangements, Employee Assistance Programs (EAP), Bonuses and Incentive Programs'",
+                 "'Health Insurance, Retirement Plans, Paid Time Off (PTO), Flexible Work Arrangements, Employee Assistance Programs (EAP)'",
+                 "'Legal Assistance, Bonuses and Incentive Programs, Wellness Programs, Employee Discounts, Retirement Plans'",
+                 "'Life and Disability Insurance, Stock Options or Equity Grants, Employee Recognition Programs, Health Insurance, Social and Recreational Activities'",
+                 "'Transportation Benefits, Professional Development, Bonuses and Incentive Programs, Profit-Sharing, Employee Discounts'",
+                 "'Tuition Reimbursement, Stock Options or Equity Grants, Parental Leave, Wellness Programs, Childcare Assistance'"]
+    ranks = dict()
+    ranks = np.abs(rank)
+    minmax = MinMaxScaler()
+    ranks = minmax.fit_transform(np.array(ranks).reshape(33, 1)).ravel()  # - преобразование данных
+    ranks = map(lambda x: round(x, 2), ranks)  # - округление элементов массива
+    my_dict = dict(zip(column_names, ranks))
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
+    print(sorted_dict)
 
 
 if __name__ == '__main__':
-    ridge_regressions()
-    recursive_feature_elimination()
-    linear_correlation()
-
-    for key, value in ranks.items():  # Вывод нормализованных оценок важности признаков каждой модели
-        ranks[key] = sorted(value.items(), key=itemgetter(1), reverse=True)
-    for key, value in ranks.items():
-        print(key)
-        print(value)
-
-    mean = {}  # - нахождение средних значений оценок важности по 3м моделям
-    for key, value in ranks.items():
-        for item in value:
-            if item[0] not in mean:
-                mean[item[0]] = 0
-            mean[item[0]] += item[1]
-    for key, value in mean.items():
-        res = value / len(ranks)
-        mean[key] = round(res, 2)
-    mean = sorted(mean.items(), key=itemgetter(1), reverse=True)
-    print("Mean")
-    print(mean)
+    #data_preprocessing()
+    decision_tree_classifier()
